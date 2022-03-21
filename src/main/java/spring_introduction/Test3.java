1ac78533b29ee98e7de40f6d0e17efa6cb0800f0
ilushaputi <putintzeff.ilya@yandex.com>
2022-03-21 19:48:01 +0300

    Lesson 9

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -6,9 +6,8 @@ public class Test3 {
     public static void main(String[] args) {
   //      Pet pet = new Dog();
 
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
-      //  Pet pet = context.getBean("myPet", Pet.class);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //  Pet pet = context.getBean("myPet", Pet.class);
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
         System.out.println(person.getSurName());
