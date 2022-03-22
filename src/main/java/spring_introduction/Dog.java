c4e8f66326c7dfed8cfa9fde602a029a32041c1c
ilushaputi <putintzeff.ilya@yandex.com>
2022-03-22 19:54:28 +0300

    Lesson 12 init and destroy

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -3,13 +3,13 @@ package spring_introduction;
 public class Dog implements Pet{
     private  String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    //public void setName(String name) {
+    //    this.name = name;
+    //}
+//
+    //public String getName() {
+    //    return name;
+    //}
 
     public Dog() {
         System.out.println("Dog bean is created");
@@ -18,5 +18,12 @@ public class Dog implements Pet{
     @Override
     public void say(){
         System.out.println("Bow-Wow");
+    }
+
+    public void init(){
+        System.out.println("Class Dog: init method");
+    }
+    public void destroy(){
+        System.out.println("Class Dog: destroy method");
     }
 }
