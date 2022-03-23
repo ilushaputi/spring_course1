56cbc4543349f171833860c73b615ec36a70d90f
ilushaputi <putintzeff.ilya@yandex.com>
2022-03-23 20:47:59 +0300

    Lesson 20 Configuration java code

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -26,12 +26,12 @@ public class Dog implements Pet{
         System.out.println("Bow-Wow");
     }
 
-    @PostConstruct
-    public void init(){
-        System.out.println("Class Dog: init method");
-    }
-    @PreDestroy
-    public void destroy(){
-        System.out.println("Class Dog: destroy method");
-    }
+  //  @PostConstruct
+  //  public void init(){
+  //      System.out.println("Class Dog: init method");
+  //  }
+  //  @PreDestroy
+  //  public void destroy(){
+  //      System.out.println("Class Dog: destroy method");
+  //  }
 }
