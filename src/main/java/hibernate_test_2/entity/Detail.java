83e706edc057f8e359a738b6b49e91c8e9b3d674
ilushaputi <putintzeff.ilya@yandex.com>
2022-03-27 13:57:04 +0300

    Hibernate Lesson 12 OneToNe Uni-directional relation

---
+++ src/main/java/hibernate_test_2/entity/Detail.java
@@ -0,0 +1,74 @@
+package hibernate_test_2.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "details")
+public class Detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    public Detail() {
+    }
+
+    //public void setId(int id) {
+    //    this.id = id;
+    //}
+
+    @Override
+    public String toString() {
+        return "Detail{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Detail(String city, String phoneNumber, String email) {
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
